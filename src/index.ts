d646f51bf27932679b1610b1ef0c86c930804965
sulemanahmad <sulemanbhatti.business@gmail.com>
2020-02-12 14:37:25 -0500

    Typescript server ready

---
+++ src/index.ts
@@ -0,0 +1,30 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const app = express();
+const port = 3000;
+app.use(bodyParser.json());
+
+app.get("/", function(req, res) {
+  res.json("sever is running");
+});
+
+app.get("/api/getdata", function(req, res) {
+  res.json("sever is running");
+});
+
+app.listen(port, err => {
+  if (!err) console.log(`server is running on port ${port}`);
+});
+
+mongoose
+  .connect(
+    "mongodb://localhost:27017/Food-Place-DB?retryWrites=true&w=majority",
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => {
+    console.log("mongo connected successfully");
+  })
+  .catch(err => {
+    console.log(err);
+  });
